3b8c303d89dc3c8ac10ed5c471dbe8fcccee35b2
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2023-08-07 11:39:40 +0100

    adding hankel transform

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use gnuplot::{AxesCommon, Caption, Color, Figure, Fix, LineWidth};
 use ndarray::{Array, Array1};
+use rustdct::{DctPlanner, TransformType4};
+use std::{f64::consts::PI, sync::Arc};
 
 fn lennard_jones(eps: f64, sig: f64, r: &Array1<f64>) -> Array1<f64> {
     let mut ir = sig / r;
@@ -27,16 +29,42 @@ fn j0(a: f64) -> f64 {
     a.sin() / a
 }
 
+fn hankel_transform(
+    prefac: f64,
+    func: &Array1<f64>,
+    grid_a: &Array1<f64>,
+    grid_b: &Array1<f64>,
+    plan: Arc<dyn TransformType4<f64>>,
+) -> Array1<f64> {
+    let mut buffer = (func * grid_a).to_vec();
+
+    plan.process_dst4(&mut buffer);
+
+    prefac * Array::from_vec(buffer) / grid_b
+}
+
 fn plot_potentials(r: &Array1<f64>, lj: &Array1<f64>, wca: &Array1<f64>) {
     let r_vec = r.to_vec();
     let lj_vec = lj.to_vec();
     let wca_vec = wca.to_vec();
     let mut fg = Figure::new();
     fg.axes2d()
-        .lines(&r_vec, &lj_vec, &[LineWidth(1.5), Caption("Lennard-Jones"), Color("black")])
+        .lines(
+            &r_vec,
+            &lj_vec,
+            &[LineWidth(1.5), Caption("Lennard-Jones"), Color("black")],
+        )
         .set_y_range(Fix(-1.5), Fix(1.0))
         .set_x_range(Fix(0.0), Fix(5.0))
-        .lines(&r_vec, &wca_vec, &[LineWidth(1.5), Caption("Weeks-Chandler-Andersen"), Color("red")]);
+        .lines(
+            &r_vec,
+            &wca_vec,
+            &[
+                LineWidth(1.5),
+                Caption("Weeks-Chandler-Andersen"),
+                Color("red"),
+            ],
+        );
     fg.show().unwrap();
 }
 
@@ -54,6 +82,12 @@ fn main() {
     let (npts, radius) = (1024, 10.24);
     let dr = radius / npts as f64;
     let dk = 2.0 * std::f64::consts::PI / (2.0 * npts as f64 * dr);
+
+    let plan: Arc<dyn TransformType4<f64>> = DctPlanner::new().plan_dst4(npts);
+
+    let rtok = 2.0 * PI * dr;
+    let ktor = dk / (2.0 * PI).powf(2.0);
+
     let r = Array::range(0.5, npts as f64, 1.0) * dr;
     let k = Array::range(0.5, npts as f64, 1.0) * dk;
     let lj_potential = lennard_jones(epsilon, sigma, &r);
@@ -65,8 +99,10 @@ fn main() {
     let j0_between = 2.0 * k.mapv(|a| j0(a * bond_length * (3.0_f64).sqrt()));
     let j0_opposite = k.mapv(|a| 2.0 * a * bond_length);
 
-    let intramolecular_correlation = 1.0 + j0_adjacent + j0_between + j0_opposite;
+    let intramolecular_correlation_kspace = 1.0 + j0_adjacent + j0_between + j0_opposite;
+    let intramolecular_correlation_rspace =
+        hankel_transform(ktor, &intramolecular_correlation_kspace, &k, &r, plan);
 
     plot_potentials(&r, &lj_potential, &wca_potential);
-    plot(&k, &intramolecular_correlation);
+    plot(&k, &intramolecular_correlation_rspace);
 }
